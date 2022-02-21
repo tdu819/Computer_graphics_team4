8b6799c4ae747b098d50a662718a929ba9336e27
Ponomarov Oleksii <testdrive819@gmail.com>
2022-02-21 19:29:54 +0200

    class MyVector was added

---
+++ project-true/MyVector.cs
@@ -0,0 +1,15 @@
+﻿namespace project_true
+{
+    public class MyVector
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public MyVector()
+        {
+            
+        }
+
+    }
+}
