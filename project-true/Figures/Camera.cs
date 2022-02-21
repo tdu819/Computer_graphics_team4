c13720cfc6bd5c774f4477ca93c327b1036b3423
Олександр Тростінецький <trostinetsky.sasha1@gmail.com>
2022-02-21 23:04:10 +0200

    add Camera and Plane classes

---
+++ project-true/Figures/Camera.cs
@@ -0,0 +1,13 @@
+﻿using project_true.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project_true.Figures
+{
+    public class Camera
+    {
+        MyPoint _center;
+        MyVector _direction;
+    }
+}
---
+++ project-true/Figures/Plane.cs
@@ -0,0 +1,13 @@
+﻿using project_true.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project_true.Figures
+{
+    public class Plane
+    {
+        MyPoint _center;
+        MyVector _normal;
+    }
+}
