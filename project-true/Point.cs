4631fac377c7a5998ec7595f0fee035220ff4b7a
Ponomarov Oleksii <testdrive819@gmail.com>
2022-02-21 19:28:17 +0200

    class Point was added

---
+++ project-true/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace project_true
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point()
+        {
+            
+        }
+    }
+}
