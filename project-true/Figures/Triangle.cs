1b016344a4c9e46c636cd292d1c4bcb6ff46e6ca
Ponomarov Oleksii <testdrive819@gmail.com>
2022-02-22 20:03:15 +0200

    add class Triangle

---
+++ project-true/Figures/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace project_true.Figures
+{
+    public class Triangle
+    {
+        public Point A { get; set; }
+        public Point B { get; set; }
+        public Point C { get; set; }
+
+        public Triangle()
+        {
+            
+        }
+        
+        public Triangle(Point a, Point b, Point c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+    }
+}
