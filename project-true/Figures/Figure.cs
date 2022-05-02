51aeb71d70a7d4fc26e4bc5bd3d2b0db88d70b63
Ponomarov Oleksii <testdrive819@gmail.com>
2022-05-02 21:55:37 +0300

    lab 1 done

--- project-true/Figures/Figure.cs
+++ project-true/Figures/Figure.cs
@@ -7,19 +7,13 @@ namespace project_true.Figures
 {
     public abstract class Figure
     {
+        
+        // Part 3 and all of the inheritance classes
         public abstract bool RayIntersect(MyPoint rayOrigin,
                                           MyPoint rayPointer,
                                           ref MyPoint IntersectionPoint);
         public abstract MyVector GetNormal(MyPoint intersectionPoint);
 
-        public override bool Equals(object? obj)
-        {
-            return base.Equals(obj);
-        }
 
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
     }
 }
